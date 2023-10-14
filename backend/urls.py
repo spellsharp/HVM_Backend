6fff3b1bf7dcdfd38149343619a3e58b94c9b056
spellsharp <svcodin22@gmail.com>
2023-10-14 19:12:05 +0530

    Added an endpoint for the media i.e images that are uploaded in the forms

--- backend/urls.py
+++ backend/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+# from django.conf.urls.static import static
+from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('hvm.urls')),
+    path('media/<path:path>/', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
