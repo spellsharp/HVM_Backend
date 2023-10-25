ace1bae0d8958976a050cd3cbe407253f256cd05
spellsharp <svcodin22@gmail.com>
2023-10-25 23:04:14 +0530

    Added static to urlpatterns in backend/urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -26,8 +26,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('hvm.urls')),
     path('media/<path:path>/', serve, {'document_root': settings.MEDIA_ROOT}),
-]
-
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
